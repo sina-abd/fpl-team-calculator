6c738ff7f05b2e2787dd44b353bcc7eae6852736
Sina <sinaedu7@gmail.com>
2024-03-06 13:43:42 +0330

    renamed some symbols and cleaned the code a bit

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 from collections import Counter
 
+home_id = input('input home team player IDs separated by an space')
+away_id = input('input away team player IDs separated by an space')
 
-portvale_id = ['14502', '17950', '1401154', '1267063', '4252172', '21057']
-away_id = ['5142', '69869', '58231', '1582403', '1864514', '522758']
-portvale_sum = 0
-portvale_players = []
-away_players= []
+home_sum = 0
+home_players = []
 away_sum = 0
-for id in portvale_id:
+away_players= []
+
+for id in home_id:
 
     livefpl_url = f'https://livefpl.net/{id}'
     fplteam_url = f'https://fpl.team/live/{id}'
@@ -26,35 +27,35 @@ for id in portvale_id:
     
     print(user_name_element.text, user_point_element.text.split()[3])
     
-    portvale_sum += int(user_point_element.text.split()[3])
+    home_sum += int(user_point_element.text.split()[3])
 
     gk_name = fplteam_soup.select_one('body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(1) > div > div > div:nth-of-type(2)')
-    portvale_players.append(gk_name.text)
+    home_players.append(gk_name.text)
 
     for i in range(1, 6):
         def_name = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type({i}) > div > div:nth-of-type(2)')
         if def_name is not None:
-            portvale_players.append(def_name.text)
+            home_players.append(def_name.text)
             cap_wid = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type({i}) > div > div:nth-of-type(1)')
             cap_wid = cap_wid.find(name="svg").get('style')
             if cap_wid is not None:
-                portvale_players.append(def_name.text)
+                home_players.append(def_name.text)
     for i in range(1, 6):
         mid_name = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type({i}) > div > div:nth-of-type(2)')
         if mid_name is not None:
-            portvale_players.append(mid_name.text)
+            home_players.append(mid_name.text)
             cap_wid = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type({i}) > div > div:nth-of-type(1)')
             cap_wid = cap_wid.find(name="svg").get('style')
             if cap_wid is not None:
-                portvale_players.append(mid_name.text)
+                home_players.append(mid_name.text)
     for i in range(1, 4):
         fwd_name = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(4) > div:nth-of-type({i}) > div > div:nth-of-type(2)')
         if fwd_name is not None:
-            portvale_players.append(fwd_name.text)
+            home_players.append(fwd_name.text)
             cap_wid = fplteam_soup.select_one(f'body > main > div:nth-of-type(2) > div > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(4) > div:nth-of-type({i}) > div > div:nth-of-type(1)')
             cap_wid = cap_wid.find(name="svg").get('style')
             if cap_wid is not None:
-                portvale_players.append(fwd_name.text)
+                home_players.append(fwd_name.text)
             
 for id in away_id:
 
@@ -102,11 +103,11 @@ for id in away_id:
             cap_wid = cap_wid.find(name="svg").get('style')
             if cap_wid is not None:
                 away_players.append(fwd_name.text)
-c_portvale_players = Counter(portvale_players)
+c_home_players = Counter(home_players)
 c_away_players = Counter(away_players)
-print(f"portvale players: {c_portvale_players}")
-print(f"total point is: {portvale_sum}")
-print(f"away players: {c_away_players}")
+print(f"home team players: {c_home_players}")
+print(f"total point is: {home_sum}")
+print(f"away team players: {c_away_players}")
 print(f"total point is: {away_sum}")
-print(f"players we got: {c_portvale_players-c_away_players}")
-print(f"players they got: {c_away_players-c_portvale_players}")
+print(f"effective players of home team: {c_home_players-c_away_players}")
+print(f"effective players of away team: {c_away_players-c_home_players}")
